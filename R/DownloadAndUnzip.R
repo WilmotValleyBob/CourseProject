906c6851d6ad7cdbc9ae464a5eb4dd7abdb9670b
WilmotValleyBob <Bob.McKillop@pei.sympatico.ca>
2015-04-25 16:17:14 -0300

    New version of download and unzip script

---
+++ R/DownloadAndUnzip.R
@@ -0,0 +1,37 @@
+#
+#
+# Getting and Cleaning Data Class project
+#
+
+#
+# Download and Unzip Datafiles
+#
+
+#    Create one R script called run_analysis.R that does the following. 
+
+# 1) Merges the training and the test sets to create one data set.
+# 2) Extracts only the measurements on the mean and standard deviation for each measurement. 
+# 3) Uses descriptive activity names to name the activities in the data set
+# 4) Appropriately labels the data set with descriptive variable names. 
+# 5) From the data set in step 4, creates a second, independent tidy data set
+#    with the average of each variable for each activity and each subject.
+#
+#   Set wworking directory
+#
+
+setwd = ("D:/Users/Bob/Bobs Courses/DataScience files//RStdio ProjectsGandCProject/Data")
+
+#
+#   Download data set zip file
+#
+
+fileurl = "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(fileurl,destfile="DataSet.zip")
+downloaddate = date()
+
+#
+#   Extract data files from .zip file
+#
+unzip("./DataSet.zip", files = NULL, list = FALSE, overwrite = TRUE,
+      junkpaths = FALSE, exdir = ".", unzip = "internal",
+      setTimes = FALSE)
